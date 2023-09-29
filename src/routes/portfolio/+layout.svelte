2a08fdfb6dbb3da8e1cbb8b20e0afd270c86ebdf
Oscar Vian Valles <oscarvianvalles@gmail.com>
2023-09-29 22:37:22 +0800

    init commit

---
+++ src/routes/portfolio/+layout.svelte
@@ -0,0 +1,11 @@
+<main class="main"><slot /></main>
+
+<style lang="scss">
+	.main {
+		width: 100%;
+		height: 100vh;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
---
+++ src/routes/portfolio/three/+page.svelte
@@ -0,0 +1,12 @@
+<section>
+	<img src="/images/3.jpg" width="900" class="img" alt="Person sweating while installing tint" />
+</section>
+
+<style lang="scss">
+	.img {
+		max-width: 50rem;
+		width: calc(100vw - 2rem);
+		max-height: calc(100vh - 2rem);
+		margin: 0 auto;
+	}
+</style>
